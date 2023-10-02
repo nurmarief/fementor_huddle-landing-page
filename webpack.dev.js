1509b4a51fab3b50542d22e1cbfa4d48f366ce8c
nurmarief <ariefmuhammad897@gmail.com>
2023-10-02 18:53:15 +0700

    initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      // favicon: './src/images/favicon-32x32.png',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
